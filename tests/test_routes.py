4f0fadee761f98a4d1f615a7d2defdb34024221c
yuzh0507 <yuzh0507@gmail.com>
2023-11-29 13:11:57 -0500

    added delete_accounts()

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -164,3 +164,9 @@ class TestAccountService(TestCase):
         new_account["name"] = "Something Known"
         resp = self.client.put(f"{BASE_URL}/{new_account['id']+1}", json=new_account)
         self.assertEqual(resp.status_code, status.HTTP_404_NOT_FOUND)
+
+    def test_delete_account(self):
+        """It should Delete an Account"""
+        account = self._create_accounts(1)[0]
+        resp = self.client.delete(f"{BASE_URL}/{account.id}")
+        self.assertEqual(resp.status_code, status.HTTP_204_NO_CONTENT)
